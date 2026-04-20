7045e1f4a0ee37cfbd37cd03aeedd0b27061b1be
sindhu-preetham <mlsindhupreetham.241ai021@nitk.edu.in>
2026-04-20 23:10:00 +0530

    Add: monotonic-array - submission-0

---
+++ Data Structures & Algorithms/monotonic-array/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        if(nums[0]>nums[1]){
+            for(int i=1;i<nums.size()-1;i++){
+                if(nums[i]<nums[i+1]){
+                    return false;
+                }
+            }
+        }else{
+            for(int i=1;i<nums.size()-1;i++){
+                if(nums[i]>nums[i+1]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+};
